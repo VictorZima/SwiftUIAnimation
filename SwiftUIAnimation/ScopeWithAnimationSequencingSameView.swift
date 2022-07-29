d77364977d8d8f14b93b7cbbeddfc3f4777edff0
VictorZima <victorzima@icloud.com>
2022-07-29 04:18:23 +0300

    Scope With Animation Sequencing Same View

---
+++ SwiftUIAnimation/ScopeWithAnimationSequencingSameView.swift
@@ -0,0 +1,65 @@
+//
+//  ScopeWithAnimationSequencingSameView.swift
+//  SwiftUIAnimation
+//
+//  Created by VictorZima on 29/07/2022.
+//
+
+import SwiftUI
+
+struct ScopeWithAnimationSequencingSameView: View {
+    @State private var expandMoreInfo = false
+    @State private var showInfo = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            TitleText("With Animation")
+            SubtitleText("Sequencing with Parent/Child")
+            
+            GeometryReader { gr in
+                VStack(spacing: 15) {
+                    HStack {
+                        Image(systemName: "info.circle")
+                            .padding(.horizontal)
+                        Text("Login Help")
+                        Spacer()
+                    }
+                    .padding(.top)
+                    
+                    VStack(spacing: 10) {
+                        Image(systemName: "slider.horizontal.3")
+                            .font(.largeTitle)
+                        Text("Appear after parent is done animating out.")
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 15).opacity(0.1))
+                    .padding(.horizontal)
+                    .opacity(self.showInfo ? 1 : 0)
+                }
+                .padding(.trailing, 12)
+                .background(RoundedRectangle(cornerRadius: 20).fill(Color.yellow).shadow(radius: 8))
+                .foregroundColor(.black)
+                .offset(x: self.expandMoreInfo ? 15 : (gr.frame(in: .global).width - 65),
+                        y: self.expandMoreInfo ? 15 : (gr.frame(in: .global).height - 65))
+                .onTapGesture {
+                    withAnimation {
+                        self.expandMoreInfo.toggle()
+                    }
+                    withAnimation(.default.delay(0.5)) {
+                        self.showInfo.toggle()
+                    }
+                }
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .font(.title)
+    }
+}
+
+struct ScopeWithAnimationSequencingSameView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScopeWithAnimationSequencingSameView()
+    }
+}
